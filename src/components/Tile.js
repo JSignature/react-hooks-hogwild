bf2580f796bd177840e4f4016f7caef9fd01eb8b
Jimmy Andreas <jimmysignature@gmail.com>
2022-06-12 21:58:38 -0400

    Card Completed

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import { React, useState } from 'react'
 
-const Tile = () => {
-  return <div>Tile</div>
+const Tile = ({ name, image, specialty, greased, weight, medal }) => {
+  const [toggle, setToggle] = useState(false)
+
+  console.log(name)
+  console.log(weight)
+  console.log(greased)
+  return (
+    <div onClick={() => setToggle(!toggle)} class="ui card">
+      <div class="image">
+        <img src={image} />
+      </div>
+      <div class="content">
+        <div class="header">{name}</div>
+        {toggle ? (
+          <>
+            <div class="description">Specialty: {specialty}</div>
+            <div class="description">Greased: {greased.value}</div>
+            <div class="description">Weight: {weight}</div>
+            <div class="description">Medal: {medal}</div>
+          </>
+        ) : null}
+      </div>
+    </div>
+  )
 }
 
 export default Tile
--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Tile from './Tile'
 
-const Tiles = hogs => {
+const Tiles = ({ hogs }) => {
+  console.log(hogs)
   return (
     <div>
+      <h1>Test</h1>
       {hogs.map(hog => (
-        <Tile key={hog.id} name={hog.name} iamge={hog.image} />
+        <Tile
+          key={hog.id}
+          name={hog.name}
+          image={hog.image}
+          specialty={hog.specialty}
+          greased
+          weight
+          medal={'highest medal achieved'}
+        />
       ))}
     </div>
   )
