fb298a86931f4b7be38c533258a421c93a790c37
Jimmy Andreas <jimmysignature@gmail.com>
2022-06-13 22:13:52 -0400

    Filtered Grease Pig

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -2,31 +2,36 @@ import { React, useState } from 'react'
 import Tile from './Tile'
 
 const Tiles = ({ hogs }) => {
-  const [filteredHogs, setFilteredHogs] = useState({ hogs })
+  const [filteredHogs, setFilteredHogs] = useState(hogs)
+  console.log(filteredHogs)
   const [checked, setChecked] = useState(false)
   // console.log(checked)
 
-  if (checked) {
-    const greasedHogs = filteredHogs.hogs.filter(hog => hog.greased)
-    setFilteredHogs(greasedHogs)
-    console.log(greasedHogs)
-  } else {
-    // setFilteredHogs(hogs)
-    console.log('unchecked')
+  function handleCheck() {
+    setChecked(!checked)
+    if (!checked) {
+      const greasedHogs = filteredHogs.filter(hog => hog.greased)
+
+      setFilteredHogs(greasedHogs)
+      console.log('checked' + greasedHogs)
+    } else {
+      setFilteredHogs(hogs)
+      console.log('unchecked')
+    }
   }
 
   return (
     <div>
       <label htmlFor="Greased">Click here to show greased pigs</label>
       <input
-        onClick={() => setChecked(!checked)}
+        onClick={handleCheck}
         type="checkbox"
         name="Greased"
         id=""
         checked={checked}
       />
 
-      {filteredHogs.hogs.map(hog => (
+      {filteredHogs.map(hog => (
         <Tile key={hog.name} hog={hog} />
       ))}
     </div>
